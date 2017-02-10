f60fdadfdc4fa615a71e4a04e961fde8caff566b
Alex Diep <alexdiep2501@gmail.com>
2017-02-10 11:41:22 -0500

    sorted list

--- install.sh
+++ install.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
 PROGRAMS=(
-	     acpi
-	     alsa-utils
-	     networkmanager
-	     ttf-dejavu
-	     ttf-hack
-	     urxvt-perls
-	     xclip
-	     xorg
-	     xorg-init
-             acpid
-             atool
-             dunst
-             feh
-             firefox
-             gvim
-             i3-wm
-             i3status
-             network-manager-applet
-             rclone
-             redshift
-             rofi
-             rxvt-unicode
-             stow
-             tlp
-             zsh
-	     evince
+            acpi
+            acpid
+            adobe-source-code-pro-fonts
+            alsa-utils
+            atool
+            dunst
+            evince
+            feh
+            firefox
+            gvim
+            i3-wm
+            i3status
+            network-manager-applet
+            networkmanager
+            rclone
+            redshift
+            rofi
+            rxvt-unicode
+            stow
+            tlp
+            ttf-dejavu
+            ttf-hack
+            urxvt-perls
+            xclip
+            xf86-video-intel
+            xorg
+            xorg-init
+            zsh
          )
 
 
