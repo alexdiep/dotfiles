6ac9c279da26070b9d1b6e0c5d65c15e2b69e999
Alex Diep <alexdiep2501@gmail.com>
2017-09-15 21:01:09 -0400

    sorted installed programs and added things

--- install.sh
+++ install.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
 
 PROGRAMS=(
-            acpi
-            acpid
-            adobe-source-code-pro-fonts
-            alsa-utils
-            atool
-            dunst
-            evince
-            feh
-            firefox
-            gvim
-            i3-wm
-            i3status
-            network-manager-applet
-            networkmanager
-            rclone
-            redshift
-            rofi
-            rxvt-unicode
-            stow
-            tlp
-            ttf-dejavu
-            ttf-hack
-            urxvt-perls
-            xclip
-            xf86-video-intel
-            xorg
-            xorg-init
-            zsh
-         )
-
+acpi
+acpid
+adobe-source-code-pro-fonts
+alsa-utils
+atool
+dunst
+evince
+feh
+firefox
+gnome-keyring
+gvim
+i3-wm
+i3status
+network-manager-applet
+networkmanager
+pavucontrol
+pulseaudio
+pulseaudio-alsa
+rclone
+redshift
+rofi
+rxvt-unicode
+seahorse
+stow
+tlp
+ttf-dejavu
+ttf-hack
+udiskie
+urxvt-perls
+xclip
+xf86-video-intel
+xorg
+xorg-xinit
+xterm
+zsh
+)
 
 echo "Installing ${PROGRAMS[*]}"
 sudo pacman -Syu ${PROGRAMS[*]}
